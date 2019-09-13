8841d472d1bcd08b580063dd01a60fcc38aa9208
lieby weintraub <liebahinda@gmail.com>
2019-09-13 21:14:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,31 @@ function getFirstSelector(selector) {
 function nestedTarget(){
   return document.querySelector("div.target");
 }
-function increaseRankBy(n) {
-  
+
+function deepestChild(){
+  let node = document.getElementById('grand-node')
+  let childNode = node.children[0]
+
+  while(childNode){
+    node = childNode
+    childNode = node.children[0];
+  }
+  return node;
+}
+
+
+
+function increaseRankBy(n){
+  const rankedList = document.querySelectorAll('.ranked-list')
+
+  for ( let i = 0, l = rankedList.length; i < l ; i++){
+
+    let children = rankedList[i].children;
+
+    for ( let j = 0, k = children.length; j <k; j++) {
+
+      children[j].innerHTML = parseInt(children[j].innerHTML)+n
+
+    }
+  }
 }
